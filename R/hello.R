782440dcd3f5f1edfa6d4549e508d69a32d2aec0
Dynamite-Crypto <dadoxpalaz@gmail.com>
2024-06-20 16:51:06 +0200

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,7 @@
 
 hello <- function() {
 
-  cat("# Vettore medie: colMeans(dati)
+  return(cat("# Vettore medie: colMeans(dati)
 # n=nrow(dati)
 # medie=t(dati)%*% rep(1,n)/n
 # matrice di correlazione: → cor(dati)
@@ -249,5 +249,5 @@ hello <- function() {
 #                    km3$betweenss/km3$totss, km4$betweenss/km4$totss,
 #                    km5$betweenss/km5$totss, km6$betweenss/km6$totss)
 # plot(c(1:6), explained.var, xlab=“N. clusters”,
-#      ylab=“% varianza spiegata”, type=“l”)","\n")
+#      ylab=“% varianza spiegata”, type=“l”)","\n"))
 }
